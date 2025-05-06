b2f8b78b05bf835509ea157716b156900d367ae6
Joe Moncrieff <shoepaste@Joes-MacBook-Pro.local>
2025-05-06 09:39:24 +0100

    refactoring and prefitfying steps

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,11 +1,25 @@
 // This script is used to display the deck list in the popup
 
+let currentBg = "lightgrey"
+function toggleBg(cardDiv) {
+    if (currentBg === "lightgrey") {
+        cardDiv.style.backgroundColor = "white";
+        currentBg = "white";
+    } else {
+        cardDiv.style.backgroundColor = "lightgrey";
+        currentBg = "lightgrey";
+    }
+}
+
 chrome.storage.local.get("deckList").then(data => {
     const deckList = document.getElementById("deck-list");
+    
     // each element in a div that has the card name and also a button to remove the card from the deckList 
     // and also remove it from the popup display
     data.deckList.forEach(card => {
         const cardDiv = document.createElement("div");
+        toggleBg(cardDiv);
+        cardDiv.classList.add("card-div");
         cardDiv.style.display = "flex";
         const paragraph = document.createElement("p");
         paragraph.style.float = "left"
@@ -13,11 +27,8 @@ chrome.storage.local.get("deckList").then(data => {
         paragraph.textContent = card;
         // align remove button to stick onto the right wall of the cardDiv      
         const removeButton = document.createElement("button");
-        removeButton.textContent = "Remove"
-        removeButton.style.marginRight = "10px";
-        removeButton.style.marginLeft = "5px";
-        removeButton.style.float = "right";
-        removeButton.style.width = "20%";
+        removeButton.classList.add("card-remove-button");
+        removeButton.textContent = "\u{274C}";
         //needs to refresh the popup to remove the card from the display
         removeButton.addEventListener("click", () => {
             chrome.storage.local.get("deckList").then(data => {
@@ -31,3 +42,17 @@ chrome.storage.local.get("deckList").then(data => {
         deckList.appendChild(cardDiv);
     });
 });
+
+// add a button to the bottom of the deck list that will copy the deckList from the chrome storage to the clipboard
+const copyButton = document.createElement("button");
+copyButton.textContent = "Copy Deck List";
+copyButton.addEventListener("click", () => {
+    chrome.storage.local.get("deckList").then(data => {
+        const deckList = data.deckList.join("\n");
+        navigator.clipboard.writeText(deckList);
+    });
+});
+
+// add the copy button to the bottom of the deck list
+const deckList = document.getElementById("deck-list");
+deckList.appendChild(copyButton);
