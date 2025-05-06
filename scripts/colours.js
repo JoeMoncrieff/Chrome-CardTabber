0d726faee8a87c9a2c1fe6b700975624b177dea4
Joe Moncrieff <shoepaste@Joes-MacBook-Pro.local>
2025-05-06 10:57:29 +0100

    added functionality buttons and design changes

---
+++ scripts/colours.js
@@ -0,0 +1,7 @@
+const colours = {
+    "background": "#78A2CC",
+    "listbg1": "#C0EFFF",
+    "listbg2": "#DEF7FF"
+}
+
+export default colours;
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,15 +1,20 @@
 // This script is used to display the deck list in the popup
 
-let currentBg = "lightgrey"
+import colours from "./colours.js";
+
+let currentBg = colours.listbg2
 function toggleBg(cardDiv) {
-    if (currentBg === "lightgrey") {
-        cardDiv.style.backgroundColor = "white";
-        currentBg = "white";
+    if (currentBg === colours.listbg1) {
+        cardDiv.style.backgroundColor = colours.listbg2;
+        currentBg = colours.listbg2;
     } else {
-        cardDiv.style.backgroundColor = "lightgrey";
-        currentBg = "lightgrey";
+        cardDiv.style.backgroundColor = colours.listbg1;
+        currentBg = colours.listbg1;
     }
 }
+
+
+
 
 chrome.storage.local.get("deckList").then(data => {
     const deckList = document.getElementById("deck-list");
@@ -18,12 +23,12 @@ chrome.storage.local.get("deckList").then(data => {
     // and also remove it from the popup display
     data.deckList.forEach(card => {
         const cardDiv = document.createElement("div");
-        toggleBg(cardDiv);
+        cardDiv.style.backgroundColor = colours.listbg2;
+        //toggleBg(cardDiv);
         cardDiv.classList.add("card-div");
         cardDiv.style.display = "flex";
         const paragraph = document.createElement("p");
-        paragraph.style.float = "left"
-        paragraph.style.width = "80%";
+        paragraph.classList.add("card-name");
         paragraph.textContent = card;
         // align remove button to stick onto the right wall of the cardDiv      
         const removeButton = document.createElement("button");
@@ -40,12 +45,14 @@ chrome.storage.local.get("deckList").then(data => {
         cardDiv.appendChild(paragraph);
         cardDiv.appendChild(removeButton);
         deckList.appendChild(cardDiv);
+        //add dotted line to the bottom of the cardDiv
+        cardDiv.style.borderBottom = "3px dotted " + colours.background;
     });
 });
 
 // add a button to the bottom of the deck list that will copy the deckList from the chrome storage to the clipboard
-const copyButton = document.createElement("button");
-copyButton.textContent = "Copy Deck List";
+const copyButton = document.getElementById("copy-deck-list");
+copyButton.textContent = "\u{1F4DD} Copy To Clipboard \u{1F4DD}";
 copyButton.addEventListener("click", () => {
     chrome.storage.local.get("deckList").then(data => {
         const deckList = data.deckList.join("\n");
@@ -53,6 +60,23 @@ copyButton.addEventListener("click", () => {
     });
 });
 
-// add the copy button to the bottom of the deck list
-const deckList = document.getElementById("deck-list");
-deckList.appendChild(copyButton);
+const clearButton = document.getElementById("clear-deck-list");
+clearButton.textContent = "\u{1F5D1} Clear All \u{1F5D1}";
+clearButton.addEventListener("click", () => {
+    chrome.storage.local.set({"deckList": []});
+    location.reload();
+});
+
+const downloadButton = document.getElementById("download-deck-list");
+downloadButton.textContent = "\u{1F4E5} Download TXT \u{1F4E5}";
+downloadButton.addEventListener("click", () => {
+    chrome.storage.local.get("deckList").then(data => {
+        const deckList = data.deckList.join("\n");
+        const blob = new Blob([deckList], { type: "text/plain" });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = "deckList.txt";
+        a.click();
+    });
+});
